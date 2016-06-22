992fdea3030da6c88bd46313e0abb7e85ec140df
Isaac Lessard <isaac.c.lessard@gmail.com>
2016-06-22 16:47:32 -0700

    :+1: update validations file

--- validations.js
+++ validations.js
@@ -3,29 +3,33 @@
 class Validator {
   constructor(validationObject) {
     this.validationObject = validationObject
+    this.errors = []
   }
 
    valid (cat) {
-    var errors = [];
     for (var key in cat) {
       if (this.validationObject.hasOwnProperty(key)) {
-        if (typeof cat[key] !== this.validationObject[key].type) {
-          errors.push(key + ' is not type ' + this.validationObject[key].type)
-        }
-        if (this.validationObject[key].hasOwnProperty('length')) {
-          if (this.validationObject[key].length.hasOwnProperty('greaterThan')) {
-            // console.log("cat:",cat[key].length)
-            // console.log('proto:',this.validationObject[key].length.greaterThan)
-            if (cat[key].length <= this.validationObject[key].length.greaterThan) {
-              errors.push(key + ' does not have length that is > ' + this.validationObject[key].length.greaterThan)
-            }
-          }
-        }
+        this.validationObject[key].hasOwnProperty('type') ? this.checkType(cat, key) : false
+        this.validationObject[key].hasOwnProperty('length') ? this.checkLength(cat, key) : false
       }
     }
-    this.errors = errors;
-    return errors.length > 0 ? false : true;
+    return this.errors.length > 0 ? false : true;
   }
+
+  checkType (obj, key) {
+    if (typeof obj[key] !== this.validationObject[key].type) {
+      this.errors.push(key + ' is not type ' + this.validationObject[key].type)
+    }
+  }
+
+  checkLength (obj, key) {
+    if (this.validationObject[key].length.hasOwnProperty('greaterThan')) {
+      if (obj[key].length <= this.validationObject[key].length.greaterThan) {
+        this.errors.push(key + ' does not have length that is > ' + this.validationObject[key].length.greaterThan)
+      } // greater than
+    } // checks length key for greaterThan value
+  } // checks for property length
+
 }
 
 module.exports = Validator;
